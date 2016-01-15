79a99c824a248cf2a265120e8780230c10536911
Daniel Häfele <haefele@xemio.net>
2016-01-15 19:26:34 +0100

    Write tests for async-binary-reader and async-binary-writer

--- src/PalmDB.Tests/Serialization/AsyncBinaryReaderTests.cs
+++ src/PalmDB.Tests/Serialization/AsyncBinaryReaderTests.cs
@@ -12,15 +12,35 @@ namespace PalmDB.Tests.Serialization
     public class AsyncBinaryReaderTests
     {
         [Fact]
-        public async Task The_ReadAsync_Method_ActuallyWorks()
+        public void The_Constructor_Throws_When_Stream_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new AsyncBinaryReader(null);
+            });
+        }
+
+        [Fact]
+        public async Task The_ReadAsync_Method_Works()
         {
             var expected = Encoding.UTF8.GetBytes("123456");
             var stream = new MemoryStream(expected);
-
             var reader = new AsyncBinaryReader(stream);
+
             var actual = await reader.ReadAsync(expected.Length);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task The_ReadAsync_Method_Throws_When_Length_Is_Negative()
+        {
+            var reader = new AsyncBinaryReader(new MemoryStream());
+
+            await Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                await reader.ReadAsync(-1);
+            });
         }
     }
 }
---
+++ src/PalmDB.Tests/Serialization/AsyncBinaryWriterTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using PalmDB.Serialization;
+using Xunit;
+
+namespace PalmDB.Tests.Serialization
+{
+    public class AsyncBinaryWriterTests
+    {
+        [Fact]
+        public void The_Constructor_Throws_When_Stream_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new AsyncBinaryWriter(null);
+            });
+        }
+
+        [Fact]
+        public async Task The_WriteAsync_Method_Works()
+        {
+            var stream = new MemoryStream();
+            var writer = new AsyncBinaryWriter(stream);
+            var expected = Encoding.UTF8.GetBytes("123456");
+
+            await writer.WriteAsync(expected);
+            var actual = stream.ToArray();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task The_WriteAsync_Method_Throws_If_Data_Is_Null()
+        {
+            var writer = new AsyncBinaryWriter(new MemoryStream());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await writer.WriteAsync(null);
+            });
+        }
+    }
+}
