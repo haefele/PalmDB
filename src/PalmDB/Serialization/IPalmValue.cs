39b002a5127e7294d6c02d64a29d31c8eebe2f22
Daniel Häfele <haefele@xemio.net>
2016-01-15 20:04:18 +0100

    Add code documentation to the IPalmValue

--- src/PalmDB/Serialization/IPalmValue.cs
+++ src/PalmDB/Serialization/IPalmValue.cs
@@ -3,9 +3,22 @@ using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// Handles reading and writing of the specified <typeparamref name="T"/> in a palm database file.
+    /// </summary>
+    /// <typeparam name="T">The type of value this class can read and write.</typeparam>
     internal interface IPalmValue<T>
     {
+        /// <summary>
+        /// Reads the <typeparamref name="T"/> using the specified <paramref name="reader"/>.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
         Task<T> ReadValueAsync(AsyncBinaryReader reader);
+        /// <summary>
+        /// Writes the specified <paramref name="value"/> using the specified <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        /// <param name="value">The value.</param>
         Task WriteValueAsync(AsyncBinaryWriter writer, T value);
     }
 }
