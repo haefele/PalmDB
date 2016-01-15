7e64b3a449126b288233d0873ea2780bfac3f3fe
Daniel Häfele <haefele@xemio.net>
2016-01-15 20:27:44 +0100

    Add code documentation to the EnumPalmValue

--- src/PalmDB/Serialization/EnumPalmValue.cs
+++ src/PalmDB/Serialization/EnumPalmValue.cs
@@ -1,12 +1,24 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// Represents a <see cref="int"/> based <see cref="Enum"/> value inside a palm database.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal class EnumPalmValue<T> : IPalmValue<T>
         where T : struct
     {
+        /// <summary>
+        /// Gets the length of this enum block.
+        /// </summary>
         public int Length { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumPalmValue{T}"/> class.
+        /// </summary>
+        /// <param name="length">The length of the enum block.</param>
         public EnumPalmValue(int length)
         {
             Guard.NotNegative(length, nameof(length));
@@ -14,6 +26,10 @@ namespace PalmDB.Serialization
             this.Length = length;
         }
 
+        /// <summary>
+        /// Reads the <typeparamref name="T"/> using the specified <paramref name="reader"/>.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
         public async Task<T> ReadValueAsync(AsyncBinaryReader reader)
         {
             Guard.NotNull(reader, nameof(reader));
@@ -23,7 +39,11 @@ namespace PalmDB.Serialization
 
             return (T)(object)(int)data;
         }
-
+        /// <summary>
+        /// Writes the specified <paramref name="value"/> using the specified <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        /// <param name="value">The value.</param>
         public async Task WriteValueAsync(AsyncBinaryWriter writer, T value)
         {
             Guard.NotNull(writer, nameof(writer));
