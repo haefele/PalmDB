7d55c25e153686a62b9dbd0d06f9350d7fd2bbdf
Daniel Häfele <haefele@xemio.net>
2016-01-15 19:20:20 +0100

    Add code comments to the async-binary-reader and async-binary-writer

--- src/PalmDB/Serialization/AsyncBinaryReader.cs
+++ src/PalmDB/Serialization/AsyncBinaryReader.cs
@@ -3,10 +3,20 @@ using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// A simple class that can be used to read from a <see cref="Stream"/> asynchronously.
+    /// </summary>
     internal class AsyncBinaryReader
     {
+        /// <summary>
+        /// Gets the underlying stream.
+        /// </summary>
         public Stream Stream { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncBinaryReader"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
         public AsyncBinaryReader(Stream stream)
         {
             Guard.NotNull(stream, nameof(stream));
@@ -14,6 +24,10 @@ namespace PalmDB.Serialization
             this.Stream = stream;
         }
 
+        /// <summary>
+        /// Reads the specified number of bytes (<paramref name="length"/>) from the underlying <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="length">The number of bytes to read.</param>
         public async Task<byte[]> ReadAsync(int length)
         {
             Guard.NotNegative(length, nameof(length));
--- src/PalmDB/Serialization/AsyncBinaryWriter.cs
+++ src/PalmDB/Serialization/AsyncBinaryWriter.cs
@@ -3,10 +3,20 @@ using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// A simple class that can be used to write to a <see cref="Stream"/> asynchronously.
+    /// </summary>
     internal class AsyncBinaryWriter
     {
+        /// <summary>
+        /// Gets the underlying stream.
+        /// </summary>
         public Stream Stream { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncBinaryWriter"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
         public AsyncBinaryWriter(Stream stream)
         {
             Guard.NotNull(stream, nameof(stream));
@@ -14,6 +24,10 @@ namespace PalmDB.Serialization
             this.Stream = stream;
         }
 
+        /// <summary>
+        /// Writes the specified <paramref name="data"/> asynchronously to the underlying <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="data">The data.</param>
         public Task WriteAsync(byte[] data)
         {
             Guard.NotNull(data, nameof(data));
