ab42b2db338427d010e9107c44f1975eeba3a084
Daniel Häfele <haefele@xemio.net>
2016-01-15 20:04:28 +0100

    Add code documentation to the StringPalmValue

--- src/PalmDB/Serialization/StringPalmValue.cs
+++ src/PalmDB/Serialization/StringPalmValue.cs
@@ -1,17 +1,34 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// Represents a <see cref="string"/> value inside a palm database.
+    /// </summary>
     internal class StringPalmValue : IPalmValue<string>
     {
+        /// <summary>
+        /// Gets the length of this string block.
+        /// </summary>
         public int Length { get; }
+        /// <summary>
+        /// Gets the encoding used to encode and decode the string.
+        /// </summary>
         public Encoding Encoding { get; }
+        /// <summary>
+        /// Gets a value indicating whether the string is terminated with a zero-byte.
+        /// </summary>
         public bool ZeroTerminated { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringPalmValue"/> class.
+        /// </summary>
+        /// <param name="length">The length of the string block.</param>
+        /// <param name="encoding">The encoding used to encode and decode the string.</param>
+        /// <param name="zeroTerminated">Whether the string is terminated with a zero-byte.</param>
         public StringPalmValue(int length, Encoding encoding, bool zeroTerminated)
         {
             Guard.NotNegative(length, nameof(length));
@@ -22,6 +39,10 @@ namespace PalmDB.Serialization
             this.ZeroTerminated = zeroTerminated;
         }
 
+        /// <summary>
+        /// Reads the <see cref="string"/> using the specified <paramref name="reader"/>.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
         public async Task<string> ReadValueAsync(AsyncBinaryReader reader)
         {
             Guard.NotNull(reader, nameof(reader));
@@ -37,7 +58,11 @@ namespace PalmDB.Serialization
 
             return this.Encoding.GetString(data);
         }
-
+        /// <summary>
+        /// Writes the specified <paramref name="value"/> using the specified <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        /// <param name="value">The value.</param>
         public Task WriteValueAsync(AsyncBinaryWriter writer, string value)
         {
             Guard.NotNull(writer, nameof(writer));
