02959c7118d3feda772f3be2f23a079b9f7d2682
Daniel Häfele <haefele@xemio.net>
2016-01-15 20:31:00 +0100

    Add code documentation to the UIntPalmValue

--- src/PalmDB/Serialization/UintPalmValue.cs
+++ src/PalmDB/Serialization/UintPalmValue.cs
@@ -3,10 +3,20 @@ using System.Threading.Tasks;
 
 namespace PalmDB.Serialization
 {
+    /// <summary>
+    /// Represents a <see cref="uint"/> value inside a palm database.
+    /// </summary>
     internal class UIntPalmValue : IPalmValue<uint>
     {
+        /// <summary>
+        /// Gets the length of this uint block.
+        /// </summary>
         public int Length { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIntPalmValue"/> class.
+        /// </summary>
+        /// <param name="length">The length of the uint block.</param>
         public UIntPalmValue(int length)
         {
             Guard.NotNegative(length, nameof(length));
@@ -14,6 +24,10 @@ namespace PalmDB.Serialization
             this.Length = length;
         }
 
+        /// <summary>
+        /// Reads the <see cref="uint"/> using the specified <paramref name="reader"/>.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
         public async Task<uint> ReadValueAsync(AsyncBinaryReader reader)
         {
             Guard.NotNull(reader, nameof(reader));
@@ -25,7 +39,11 @@ namespace PalmDB.Serialization
 
             return BitConverter.ToUInt32(data, 0);
         }
-
+        /// <summary>
+        /// Writes the specified <paramref name="value"/> using the specified <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        /// <param name="value">The value.</param>
         public async Task WriteValueAsync(AsyncBinaryWriter writer, uint value)
         {
             Guard.NotNull(writer, nameof(writer));
